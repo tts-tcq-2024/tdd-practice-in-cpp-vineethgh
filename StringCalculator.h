f80024ddcd16f9e26059ec9c5136663d47a4f2f2
vineethgh <ghavaladvineeth@gmail.com>
2024-09-26 12:17:24 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,16 @@
-
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+ 
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <regex>
+ 
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+};
+ 
+#endif
